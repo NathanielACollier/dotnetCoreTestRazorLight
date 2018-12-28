97826f32909d6ccb29116581ab633c062902c200
Nathaniel Collier <nathaniel.a.collier@gmail.com>
2018-12-28 16:29:28 -0600

    Add project files.

---
+++ dotnetCoreTestRazorLight/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace dotnetCoreTestRazorLight
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
